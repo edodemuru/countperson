a4129dc0638d4e932b38043cd71643488a026dab
Edoardo Demuru <edodemuru@libero.it>
2018-09-27 18:40:23 +0200

    Socket

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,25 @@
 
 #include <string.h>
 
+#include "freertos/FreeRTOS.h"//kernel di sistema operativo in real-time usato nei dispositivi embedded
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+
+
+#define BIT0 (1 << 0)
+#define SSID "ESP_32"
+#define PASSPHARSE "12345678"//ssid e password della rete a cui mi voglio collegare
+#define MESSAGE "HelloTCPServer"
+#define TCPServerIP "192.168.1.1" //ip del server nella rete in questione
+
+
 //#define maxCh 13 //max Channel -> US = 11, EU = 13, Japan = 14
 #define	WIFI_CHANNEL_MAX		(13)
 #define	LED_GPIO_PIN			GPIO_NUM_4
@@ -176,17 +195,104 @@ void insert(Tarray* a, wifi_ieee80211_packet_t x){
   
 }
 
+static EventGroupHandle_t wifi_event_group;//variabile che identifica un gruppo di eventi wifi(RTOS)
+const int CONNECTED_BIT = BIT0;
+static const char *TAG="tcp_client";
+void wifi_connect(){//collega la scheda alla rete wifi specificata
+    wifi_config_t cfg = {
+      .sta ={SSID,PASSPHARSE}
+    };      
+    /*wifi_config_t cfg = {
+        .sta = {
+            .ssid = SSID,
+            .password = PASSPHARSE,
+        },
+    };*/
+    ESP_ERROR_CHECK( esp_wifi_disconnect() );//mi disconnetto dalla rete alla quale eventualmente mi ero collegato in precedenza
+    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &cfg) );//configurazione
+    ESP_ERROR_CHECK( esp_wifi_connect() );//mi connetto alla wifi
+}
+
+
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
-  //system_event_ap_probe_req_rx_t info_dispositivo;
-
-  if (event->event_id == SYSTEM_EVENT_AP_PROBEREQRECVED)
-  {
-    //system_event_ap_probe_req_rx_t *list_ap_probereqrecved = (system_event_ap_probe_req_rx_t *) malloc(sizeof(system_event_ap_probe_req_rx_t) *info_dispositivo);
-  }
+ switch(event->event_id) {//analizzo l'id relativo all'evento
+    case SYSTEM_EVENT_STA_START://l'esp32 si è avviata
+        wifi_connect();//collego l'esp32 alla wifi
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP://l'esp32 ha ricevuto l'IP dall'access point a cui si è connesso
+        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);//setto il bit in modo da sbloccare i relativi task bloccati (???)
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED://l'esp32 si è disconnesso dall'access point
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);//pulisco i bit
+        break;
+    default:
+        break;
+    }
   return ESP_OK;
 }
+
+static void initialise_wifi(void)//funzione di inizializzazione del modulo wifi
+{
+    esp_log_level_set("wifi", ESP_LOG_NONE); // disable wifi driver logging
+    tcpip_adapter_init();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+}
+
+void tcp_client(void *pvParam){
+    ESP_LOGI(TAG,"tcp_client task started \n");//stampa in output
+    /*inizializzazione socket*/
+    struct sockaddr_in tcpServerAddr;
+    tcpServerAddr.sin_addr.s_addr = inet_addr(TCPServerIP);
+    tcpServerAddr.sin_family = AF_INET;
+    tcpServerAddr.sin_port = htons( 3010 );//3010 è la porta alla quale intendiamo collegarci
+    int s, r;
+    char recv_buf[64];//buffer di ricezione
+    while(1){
+        xEventGroupWaitBits(wifi_event_group,CONNECTED_BIT,false,true,100);
+        s = socket(AF_INET, SOCK_STREAM, 0);//creo il socket
+        if(s < 0) {//errore nella funzione socket
+            ESP_LOGE(TAG, "... Failed to allocate socket.\n");
+            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... allocated socket\n");
+         if(connect(s, (struct sockaddr *)&tcpServerAddr, sizeof(tcpServerAddr)) != 0) {//mi connetto al server
+            //codice eseguito in caso di errore nella connect
+            ESP_LOGE(TAG, "... socket connect failed errno=%d \n", errno);
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... connected \n");
+        if( write(s , MESSAGE , strlen(MESSAGE)) < 0)//invio il messaggio
+        {
+            ESP_LOGE(TAG, "... Send failed \n");
+            close(s);
+            vTaskDelay(4000 / portTICK_PERIOD_MS);
+            continue;
+        }
+        ESP_LOGI(TAG, "... socket send success");
+        do {
+            bzero(recv_buf, sizeof(recv_buf));
+            r = read(s, recv_buf, sizeof(recv_buf)-1);
+            for(int i = 0; i < r; i++) {
+                putchar(recv_buf[i]);
+            }
+        } while(r > 0);
+        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d\r\n", r, errno);
+        close(s);
+        ESP_LOGI(TAG, "... new request in 5 seconds");
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+    ESP_LOGI(TAG, "...tcp_client task closed\n");
+}
+
 
 const char *
 wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type)
@@ -268,6 +374,7 @@ void setup() {
   //nvs_flash_init();
   tcpip_adapter_init();
   ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+   wifi_event_group = xEventGroupCreate();//creo un nuovo gruppo di eventi
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
 	//ESP_ERROR_CHECK( esp_wifi_set_country(&wifi_country) );/* set country for channel range [1, 13] */
@@ -275,6 +382,8 @@ void setup() {
   ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
   ESP_ERROR_CHECK( esp_wifi_start() );
 
+  initialise_wifi();
+  
   //Set promiscuous mode
   esp_wifi_set_promiscuous(true);
   //esp_wifi_set_promiscuous_filter(&filt); //Filter mac address??
@@ -326,6 +435,8 @@ void loop() {
     //dopo aver inviato i pacchetti faccio la destroy
      /*array_destroy(&a); */
     
+    xTaskCreate(&tcp_client,"tcp_client",4048,NULL,5,NULL);//creo un task e lo aggiungo alla lista dei task pronti ad essere eseguiti
+
     wifi_sniffer_set_channel(curChannel); //Change channel
     curChannel = (curChannel % WIFI_CHANNEL_MAX) + 1; //Set next channel
     //Send something over bluetooth connection
